c75f0b54001d1463d2d0b8559050b487bf21e9f1
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-10-06 10:38:54 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,24 @@
+package protect;
+//here we cretaed pacakage as protect which that organizes a set of related classes and interfaces.
+import java.io.IOException;
+//ioexception is a class of exception that was raised due to inptu or output
+import different.Method;
+
+public class First {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		//public : it can be called from anywhere.
+		//static : doesn't belong to a specific object.
+		//void : returns to no value.
+		//main : is special because it will start the program.
+		Method m=new Method();//taking an object
+	
+		m.accept();
+		
+	//here it is accepting the object class 
+	}
+
+}
+
+
---
+++ Method.java
@@ -0,0 +1,28 @@
+package different;
+//here we cretaed pacakage as protect which that organizes a set of related classes and interfaces
+
+public class Method {
+
+	public void accept() {
+		// TODO Auto-generated method stub
+		
+		String name="srujan";
+		//private modifier specifies that the member can only be acceses in its own class
+		//static used to prepare a field,method or inner classes as a class field.
+		//String is sequence of character	
+			int batchnumber=5007;
+		//int holds the numbers
+		{
+		//accept gives connect between the client and server.
+		//throws statement throws an exception.
+			//here creating main method which throws an exception
+			// TODO Auto-generated method stub
+			System.out.println("name:"+name);
+			//prints the statements
+			System.out.println("batch number:"+batchnumber);
+		}
+
+	
+	}
+
+}
